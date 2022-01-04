10955cac443c0f39b21c0038b21715cd57f442e9
DevZhin <dndn0514@naver.com>
2022-01-04 17:48:22 +0900

    2022-01-04
    이동 애니메이션 구현됨

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject target; // 카메라가 따라갈 대상
+    public float moveSpeed = 5.0f; // 카메라가 따라갈 속도
+    private Vector3 targetPosition; // 대상의 현재 위치
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        // 대상이 있는지 체크
+        if (target.gameObject != null)
+        {
+            // this는 카메라를 의미 (z값은 카메라값을 그대로 유지)
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+            
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,34 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float moveSpeed = 1.0f; // 플레이어 이동 속도
+    float Vertical; // 위쪽, 아래쪽 방향값을 받는 변수
+    float Horizontal; // 왼쪽, 오른쪽 방향값을 받는 변수
 
-    Vector3 position;
-    public float Speed = 2.0f;
-    public string Rotate;
+    public Animator animator; // Animator 속성 변수 생성
+
+    public SpriteRenderer rend; // SpriteRenderer 속성 변수 생성
 
     private void Start()
     {
-        position = transform.position;
+        animator = GetComponent<Animator>(); // animator 변수를 Player의 Animator 속성으로 초기화
     }
 
+    private void FixedUpdate()
+    {
+        Move(); // 플레이어 이동
 
-    void Update()
+    }
+
+    void Move()
     {
 
-        position.x += Speed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
-        position.y += Speed * Time.deltaTime * Input.GetAxisRaw("Vertical");
 
-        /*if (Input.GetKeyDown(KeyCode.Space))
+        Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        
+
+        if (Horizontal != 0)
         {
-            position.x += Speed * 60 * Time.deltaTime * Input.GetAxisRaw("Horizontal");
-            position.y += Speed * 60 * Time.deltaTime * Input.GetAxisRaw("Vertical");
-        }*/
+            animator.SetBool("walk_h", true);
+            animator.SetBool("walk_down", false);
+            animator.SetBool("walk_up", false);
 
-        transform.position = position;
+            if (Horizontal > 0)
+            {
+                rend.flipX = false; // Player의 Sprite를 좌우반전시키는 함수 , true일 때 반전
+            }
+            else
+            {
+                rend.flipX = true;
+            }
+        }
+        else
+        {
+            animator.SetBool("walk_h", false);
+        }
 
+        if (Vertical < 0)
+        {
+            animator.SetBool("walk_down", true);
+            animator.SetBool("walk_up", false);
+        }
+        else if (Vertical > 0)
+        {
+            animator.SetBool("walk_up", true);
+            animator.SetBool("walk_down", false);
+        }
+        else
+        {
+            animator.SetBool("walk_up", false);
+            animator.SetBool("walk_down", false);
+        }
+
+        Vector3 dir = (Vertical * Vector3.up) + (Horizontal * Vector3.right); // transform.Translate() 변수의 자료형을 맞추기 위해 생성한 새로운 Vector3 변수 생성
+        this.transform.Translate(dir * moveSpeed * Time.deltaTime); // Player 오브젝트 이동 함수
     }
-
 }
